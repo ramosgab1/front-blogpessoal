b8b98b3afc80695196dcff29acdffd7ac83faf42
ramosgab1 <gabrieller_david@hotmail.com>
2024-02-26 21:12:06 -0300

    Correção padding temas.

--- src/components/temas/cardTemas/CardTemas.tsx
+++ src/components/temas/cardTemas/CardTemas.tsx
@@ -13,16 +13,16 @@ interface CardTemasProps {
 function CardTemas({ tema }: CardTemasProps) {
     return (
         <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-            <header className='py-2 px-6 bg-indigo-600 text-white font-bold text-2xl'>Tema</header>
+            <header className='py-2 px-6 bg-red-300 text-white font-bold text-2xl'>Tema</header>
             
             {/* Agora podemos usar a props para funcionamento do componente e aqui onde antes estava a descrição estática de "DESCRIÇÃO TEMA:" substituímos pelo código { tema.descricao } que trará um texto dinâmico.  */}
 
-            <p className='p=8 text-3xl bg-slate-200 h-full'>{ tema.descricao }</p>
+            <p className='p-8 text-3xl bg-slate-200 h-full'>{ tema.descricao }</p>
             <div className='flex'>
                 {/* Aqui precisamos passar o link para a edição de tema, ou seja, a rota editar. Sendo assim, o que era apenas Link to='' deve virar Link to='/editarTema'. OBS IMPORTANTE!! NESSE LINK TO USAMOS CRASE AO INVÉS DE ASPA PQ A ROTA É DA API. CRASE = ROTA API, ASPAS = ROTA INTERNA DO ROUTER-DOM!!*/}
 
             
-               <Link to={`/editarTema/${ tema.id }`} className='w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800 flex items-center justify-center py-2'>
+               <Link to={`/editarTema/${ tema.id }`} className='w-full text-slate-100 bg-green-400 hover:bg-green-700 flex items-center justify-center py-2'>
                 <button>Editar</button>
                 </Link> 
 
